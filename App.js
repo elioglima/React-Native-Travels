9646c258aaf73d69a2f11c7fddb1f60b25831544
Elio Gonçalves de Lima <elio.lima@MacBook-Pro-de-Elio.local>
2019-10-28 20:24:39 -0300

    ajuste

--- App.js
+++ App.js
@@ -8,106 +8,53 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
+  FlatList,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+import {data} from './src/data';
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.titulo}>Explore</Text>
+          <View style={styles.foto}>
+            {data.map(reg => {
+              return (
+                <ImageBackground
+                  source={reg.src}
+                  style={styles.backgroundImage}>
+                  <Text style={styles.titulo}>{reg.name}</Text>
+                </ImageBackground>
+              );
+            })}
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {},
+  titulo: {
+    flex: 1,
+    color: '#344',
+    flexDirection: 'column',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  foto: {
+    flex: 1,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  backgroundImage: {
+    alignSelf: 'stretch',
+    height: 100,
   },
 });
 
---
+++ src/data/index.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    src: require('../assets/images/images1.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+  {
+    src: require('../assets/images/images2.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+  {
+    src: require('../assets/images/images3.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+
+  {
+    src: require('../assets/images/images5.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+  {
+    src: require('../assets/images/images6.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+  {
+    src: require('../assets/images/images7.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+
+  {
+    src: require('../assets/images/images9.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+  {
+    src: require('../assets/images/images10.jpeg'),
+    name: 'Brasil',
+    desc: '',
+  },
+];
